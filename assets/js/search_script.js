5fdbcecdfc76cc139c39d44605c121e3ca48a4ce
gisagisa <gisanimr1010@gmail.com>
2020-12-09 14:23:24 +0700

    adding time remainings

--- assets/js/search_script.js
+++ assets/js/search_script.js
@@ -1,8 +1,7 @@
 //ambil elemen 
 
 var keyword = document.getElementById('keyword');
-var tombolCari = document.getElementById('tombol_cari');
-var list_teman = document.getElementById('list_teman');
+var lihat_project = document.getElementById('lihat_project');
 
 //tambahkan event ketika keyword ditulis
 
@@ -11,21 +10,16 @@ keyword.addEventListener('keyup', function()
     //buat object ajax
     var ajax = new XMLHttpRequest();
     
-    
     //cek kesiapan ajax
     ajax.onreadystatechange=function(){
         if(ajax.readyState==4 && ajax.status==200)
         {
-            var myobj = JSON.parse(ajax.responseText);
-            list_teman.innerHTML==myobj.nama_lengkap;
+            lihat_project.innerHTML=ajax.responseText;
         }
     }
 
     //eksekusi ajax
-    ajax.open('GET','../assets/ajax/list_teman.php?keyword=' + keyword.value,true);
+    ajax.open('GET','../assets/ajax/search_project.php?keyword=' + keyword.value,true);
     ajax.send();
 });
-tombolCari.addEventListener('click', function()
-{
 
-});
--- view/dashboard.php
+++ view/dashboard.php
@@ -1,26 +1,26 @@
-<?php 
-   session_start();
-   if (!isset($_SESSION['nama'])){
-       header("Location: login.php");
-   }
-?> 
-<?php 
-    //cek status yang dikirimkan file action
-    //if(isset($_GET['status'])){
-    //    if($_GET['status'] == "hapussuccess")
-    //    {
-    //        echo 
-    //            '<script>
-    //            alert("Project berhasil di hapus");
-    //            </script>';
-    //    }
-    //    else{
-    //        echo 
-    //        '<script>
-    //        alert("Project gagal dihapus, silahkan coba lagi");
-    //        </script>';
-    //    }
-    //}
+<?php
+session_start();
+if (!isset($_SESSION['nama'])) {
+    header("Location: login.php");
+}
+?>
+<?php
+//cek status yang dikirimkan file action
+//if(isset($_GET['status'])){
+//    if($_GET['status'] == "hapussuccess")
+//    {
+//        echo 
+//            '<script>
+//            alert("Project berhasil di hapus");
+//            </script>';
+//    }
+//    else{
+//        echo 
+//        '<script>
+//        alert("Project gagal dihapus, silahkan coba lagi");
+//        </script>';
+//    }
+//}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -30,8 +30,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="../assets/css/new_project.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
-        integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
 
@@ -47,8 +46,7 @@
     <div class="container mt-5 pb-4 mb-3" id="kotak">
         <nav class="navbar navbar-expand-lg mt-2 mx-2">
             <a class="navbar-brand" href="#">Projek</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
-                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <i class="fas fa-bars"></i>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
@@ -59,13 +57,13 @@
                 </div>
                 <h5>Hello, <?php
 
-                // Tampilin Nama Depan
-                // $nama = explode(" ", $_SESSION['nama']);
-                // echo $nama[0];
+                            // Tampilin Nama Depan
+                            // $nama = explode(" ", $_SESSION['nama']);
+                            // echo $nama[0];
 
-                // Tampilin nama full
-                echo $_SESSION['nama'];
-                ?> !<i class="fas fa-user ml-2"></i></h5>
+                            // Tampilin nama full
+                            echo $_SESSION['nama'];
+                            ?> !<i class="fas fa-user ml-2"></i></h5>
             </div>
         </nav>
 
@@ -78,47 +76,62 @@
                             <i class="fas fa-search"></i>
                         </button>
                     </div>
-                    <input class="cari form-control rounded-pill" type="search" placeholder="Cari" aria-label="Search">
+                    <input id="keyword" class="cari form-control rounded-pill" type="search" placeholder="Cari" aria-label="Search">
                 </div>
             </div>
         </div>
 
         <!-- baris untuk kotak" kecil -->
         <div class="row mt-3 mb-5 ml-4" id="lihat_project">
-        <?php 
+            <?php
             //memanggil file koneksi
-            require_once ("../config/koneksi.php");
+            require_once("../config/koneksi.php");
 
             //mencari data yang project dari database
             $id = $_SESSION['id_user'];
-            $cek_data_query = "SELECT * FROM data_project WHERE id_user='$id'";
-            $data = mysqli_query($conn, $cek_data_query) or die (mysqli_error($conn));
+            $cek_data_query = "SELECT * FROM data_project WHERE id_user='$id' ORDER BY `data_project`.`deadline` DESC";
+            $data = mysqli_query($conn, $cek_data_query) or die(mysqli_error($conn));
 
             //menampilkan data project
-            while($result = mysqli_fetch_assoc($data))
-            {
-        ?>
-        <!-- kotak" kecil -->
-            <div class="col-md-3 rounded-lg mx-4 py-2 my-3  shadow bg-white" id="kotak_kecil">
-                <a href="edit_project.php?id_project=<?= $result['id_project'] ?>"><i class="fas fa-edit  float-right"></i></a>
-                <a href="lihat_project.php?id_project=<?= $result['id_project'] ?>"><i class="fas fa-eye  float-right mr-2"></i></a>
-                <a href="../config/hapus_project.php?id_project=<?= $result['id_project']?>"><i class="fas fa-trash float-right mr-2"></i></a>
-                <br>
-                <h4 ><?= $result['title'] ?></h4>
-                <p ><?= $result['description'] ?></p>
-                <button class="float-left rounded-pill px-2"><?= $result['deadline'] ?></button>
-                <i class="fas fa-user bg-white float-right"></i>
-            </div>
-        <?php } ?>
+            while ($result = mysqli_fetch_assoc($data)) {
+            ?>
+                <!-- kotak" kecil -->
+                <div class="col-md-3 rounded-lg mx-4 py-2 my-3  shadow bg-white" id="kotak_kecil">
+                    <a href="edit_project.php?id_project=<?= $result['id_project'] ?>"><i class="fas fa-edit  float-right"></i></a>
+                    <a href="lihat_project.php?id_project=<?= $result['id_project'] ?>"><i class="fas fa-eye  float-right mr-2"></i></a>
+                    <a href="../config/hapus_project.php?id_project=<?= $result['id_project'] ?>"><i class="fas fa-trash float-right mr-2"></i></a>
+                    <br>
+                    <h4><?= $result['title'] ?></h4>
+                    <p><?= $result['description'] ?></p>
+                        <?php
+                        $deadline = new DateTime($result["deadline"]);
+                        $time_now = new DateTime();
+                     
+                        if ($deadline > $time_now) {
+                            $time_difference = $deadline->diff($time_now)->format("%a" . " days left");
+                        ?>
+                            <button class="float-left rounded-pill px-2"><?= $time_difference ?></button>
+                        <?php
+                        } else {
+                            $time_difference = $deadline->diff($time_now)->format("%a" . " days ago");
+                        ?>
+                            <button class="float-left rounded-pill px-2" style="color: tomato;"><?= $time_difference ?></button>
+                        <?php
+                        }
+                        ?>
+                    <i class="fas fa-user bg-white float-right"></i>
+                </div>
+            <?php } ?>
         </div>
-        
+
 
         <div class="d-flex align-items-end flex-column">
             <a href="new_project.php"><button class="btn btn-primary" id="tombol" type="button">Proyek Baru</button></a>
         </div>
 
     </div>
-<script src="../assets/js/dashboard_script.js"></script>
+    <script src="../assets/js/dashboard_script.js"></script>
+    <script src="../assets/js/search_script.js"></script>
 </body>
 
 </html>
